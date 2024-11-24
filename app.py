420a14e3957ee66b4221a667de69e32c78362e05
Ralfs <ralfs.stabins2006@gmail.com>
2024-11-24 14:26:50 +0200

    optimized version

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 # Find roots using Vieta's formulas
 
 b = int(input("Enter -b: "))
-
 c = int(input("Enter c: "))
 
 found = False
 for i in range(-100, 101):
+    if found:
+        break
     for j in range(-100, 101):
         if i == 0 or j == 0:
             continue
-        if i * j == c:
-            with open("log.txt", "a") as f:
-                f.write(f"[ i ({i}) ] + [ j ({j}) ] = [ {i + j} ]\n")
-                f.write(f"[ i ({i}) ] * [ j ({j}) ] = [ {i * j} ]\n")
-                f.write("\n")
         if i + j == b and i * j == c:
             print(f"x1 = {i}, x2 = {j}")
             found = True
             break
-    if found:
-        break
 
 if not found:
     print("No roots found")
